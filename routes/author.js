0e4bbf80cedadc0171c8f07bfc909eea13c1be7e
Alejandro Gómez <a.gomezm@alumnos.upm.es>
2017-05-07 23:48:44 +0200

    Despliegue en Heroku

--- routes/author.js
+++ routes/author.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req, res, next){
-	res.render("author",{title: "creditos", header: "creditos"})
+	res.render("author");
 });
 
 module.exports = router;
